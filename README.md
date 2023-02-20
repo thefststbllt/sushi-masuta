8eed396130ba541ca92f1bf23f5fca4861def8f8
thefststbllt <bel12pasha@yandex.ru>
2023-02-20 13:59:29 +0300

    init project with basic structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# sushi-masuta
 Simple React App of Japanese food delivery service.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h2>Here we can start!</h2>
+    </div>
+  );
+}
+
+export default App;
